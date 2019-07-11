ce4b1a213ce2f37e899057dd127e99e03655eccd
shun123456 <shun_234@163.com>
2019-07-11 09:53:56 +0800

    init: iconfont

---
+++ src/page/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    123
+    <svg-icon icon-class="WIFI"></svg-icon>
+    <svg-icon icon-class="小微城市一卡通云平台"></svg-icon>
+  </div>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
